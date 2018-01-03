ee65c2fa2ba314dde695fc84a38b97f8f07ad3f7
Olivier Declementi <Atheryl@gmail.com>
2018-01-03 15:48:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var handleRequest = function(request, response) {
 	fs.readFile('version.txt', 'utf8', function (err, version) {
 		fs.readFile('index.html', 'utf8', function (err, html) {
 	    	response.writeHead(200);
-	   // response.end("Hello World!");
-	   // 	response.end(os.hostname() + ' ' + version);
 	   		html = html.replace('NODE_NAME', os.hostname());
 			html = html.replace('VERSION_NUMBER', version);
 	   		response.end(html);
