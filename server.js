32c56d129700e218740b43349e0674fb0f43a353
Olivier Declementi <Atheryl@gmail.com>
2018-02-06 15:37:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var http = require('http');
+var url = require('url');
 var os = require('os');
 var fs = require('fs');
 
 var handleRequest = function(request, response) {
+	
+	var path = url.parse(request.url).pathname;
+	var apiSampleRes = [
+		{
+		    name: "name",
+		    year: "year",
+		    region: "ukraine"
+		},
+		{
+		    name: "name",
+		    year: "year",
+		    region: "ukraine"
+		}
+	];
+	if (path == "/apiSample") {
+		response.writeHead(200, {"Content-Type": "application/json"});
+		response.end(JSON.stringify(apiSampleRes));
+	}
+	
 	fs.readFile('version.txt', 'utf8', function (err, version) {
 		fs.readFile('index.html', 'utf8', function (err, html) {
 	    	response.writeHead(200);
@@ -11,8 +31,7 @@ var handleRequest = function(request, response) {
 	   		response.end(html);
 		});
 	});
-  
 }
+
 var www = http.createServer(handleRequest);
 www.listen(8080);
-
